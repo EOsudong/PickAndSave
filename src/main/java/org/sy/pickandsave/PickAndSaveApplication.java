7e46362ff60d9681da5c7e3b64bc53a29c1b04e3
어 수 동 <sttr8285@naver.com>
2026-08-20 18:02:37 +0900

    spring boot create

---
+++ src/main/java/org/sy/pickandsave/PickAndSaveApplication.java
@@ -0,0 +1,13 @@
+package org.sy.pickandsave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickAndSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickAndSaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sy/pickandsave/PickAndSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sy.pickandsave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickAndSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
